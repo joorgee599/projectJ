dffd8d203ea244aedda43ede6d5fa7d24a04877f
joorgee599 <joorgee599@gmail.com>
2024-04-13 03:19:42 -0500

    layout y login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.login')
+
+@section('content')
+    
+
+<div class="container">
+    <form action="" >
+        <h1>Login</h1> 
+        <input type="text" name="email" id="email" class="form-control">
+
+        <input type="text" name="password" id="password" class="form-control">
+    </form>
+</div>
+@endsection
